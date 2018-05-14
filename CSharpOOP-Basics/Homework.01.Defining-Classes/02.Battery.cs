04088d4820f4f49050a930b56a222c8cea13341a
Hristo-R <hristo.razpopov@abv.bg>
2018-05-14 17:34:39 +0300

    added CSharp OOP-Basics/Homework.01.Defining-Classes

---
+++ CSharpOOP-Basics/Homework.01.Defining-Classes/02.Battery.cs
@@ -0,0 +1,68 @@
+﻿namespace Problem_02.LaptopShop
+{
+    using System;
+
+    public class Battery
+    {
+        private string batteryType;
+        private string batteryLife;
+
+        public Battery()
+        {
+        }
+
+        public Battery(string batteryType)
+        {
+            this.BatteryType = batteryType;
+        }
+
+        public Battery(string batteryType, string batteryLife) : this(batteryType)
+        {
+            this.BatteryLife = batteryLife;
+        }
+
+        public string BatteryType
+        {
+            get
+            {
+                return this.batteryType;
+            }
+
+            set
+            {
+                if (value != null && string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.batteryType = value;
+            }
+        }
+
+        public string BatteryLife
+        {
+            get
+            {
+                return this.batteryLife;
+            }
+
+            set
+            {
+                if (value != null && double.Parse(value) < 0)
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.batteryLife = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "battery: {0}\nbattery life: {1} hours\n",
+                this.BatteryType, 
+                this.BatteryLife);
+        }
+    }
+}
---
+++ CSharpOOP-Basics/Homework.01.Defining-Classes/02.LaptopShop.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem_02.LaptopShop
+{
+    using System;
+
+    public class LaptopShop
+    {
+        public static void Main()
+        {
+            Laptop laptopLevovo = new Laptop(
+                "Lenovo Yoga 2 Pro",
+                "Lenovo",
+                "Intel Core i5-4210U (2-core, 1.70 - 2.70 GHz, 3MB cache)",
+                "8 GB",
+                "Intel HD Graphics 4400",
+                "128GB SSD",
+                "13.3\" (33.78 cm) – 3200 x 1800 (QHD+), IPS sensor display",
+                "Li-Ion, 4-cells, 2550 mAh",
+                "4,5",
+                2259.00M);
+
+            Battery batteryLenovo = new Battery("Li-Ion, 4-cells, 2550 mAh", "4,5");
+
+            Laptop laptopHP = new Laptop("HP 250 G2", 699.00M);
+            Battery batteryHP = new Battery();
+
+            Console.WriteLine(laptopLevovo);
+            Console.WriteLine();
+            Console.WriteLine(laptopHP);
+        }
+    }
+}
