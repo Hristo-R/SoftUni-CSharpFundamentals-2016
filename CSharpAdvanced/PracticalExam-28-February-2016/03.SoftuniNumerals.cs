2e1b1e06ac3b82a449604edfc8a18717379b2357
Hristo-R <hristo.razpopov@abv.bg>
2018-05-14 13:06:05 +0300

    added Practical Exam - 28 February 2016

---
+++ CSharpAdvanced/PracticalExam-28-February-2016/03.SoftuniNumerals.cs
@@ -0,0 +1,50 @@
+﻿namespace _03.SoftuniNumerals
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class SoftuniNumerals
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string patern = @"(aa)|(aba)|(bcc)|(cc)|(cdc)";
+            Regex regex = new Regex(patern);
+            MatchCollection collection = regex.Matches(input);
+            StringBuilder result = new StringBuilder();
+
+            foreach (Match match in collection)
+            {
+                switch (match.ToString())
+                {
+                    case "aa":
+                        result.Append("0");
+                        break;
+                    case "aba":
+                        result.Append("1");
+                        break;
+                    case "bcc":
+                        result.Append("2");
+                        break;
+                    case "cc":
+                        result.Append("3");
+                        break;
+                    case "cdc":
+                        result.Append("4");
+                        break;
+                }
+            }
+            
+            BigInteger number = 0;
+            for (int index = 0; index < result.Length; index++)
+            {
+                BigInteger nextDigit = result[result.Length - 1 - index] - '0';
+                number += nextDigit * BigInteger.Pow(5, index);
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ CSharpAdvanced/PracticalExam-28-February-2016/04.Events.cs
@@ -0,0 +1,69 @@
+﻿namespace _04.Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Events
+    {
+        private static Dictionary<string, SortedDictionary<string, List<string>>> result =
+        new Dictionary<string, SortedDictionary<string, List<string>>>();
+
+        private static string[] locations;
+
+        public static void Main()
+        {
+            ReadData();
+            PrintResult();
+            Console.ReadLine();
+        }
+
+        private static void PrintResult()
+        {
+            foreach (string validLocation in locations)
+            {
+                int count = 1;
+
+                if (result.ContainsKey(validLocation))
+                {
+                    Console.WriteLine("{0}:", validLocation);
+                    foreach (var location in result[validLocation])
+                    {
+                        Console.WriteLine("{0}. {1} -> {2}", count, location.Key, string.Join(", ", location.Value.OrderBy(x => x)));
+                        count++;
+                    }
+                }
+            }
+        }
+
+        private static void ReadData()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            string pattern = @"^\s*#([a-zA-Z]+):\s*@([a-zA-Z]+)\s*(([0-1]\d?|2[0-3]?):[0-5]\d?)$";
+
+            for (int i = 0; i < rows; i++)
+            {
+                Match match = Regex.Match(Console.ReadLine(), pattern);
+
+                string person = match.Groups[1].ToString();
+                string location = match.Groups[2].ToString();
+                string hour = match.Groups[3].ToString();
+
+                if (!result.ContainsKey(location))
+                {
+                    result[location] = new SortedDictionary<string, List<string>>();
+                }
+
+                if (!result[location].ContainsKey(person))
+                {
+                    result[location][person] = new List<string>();
+                }
+
+                result[location][person].Add(hour);
+            }
+
+            locations = Console.ReadLine().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x).ToArray();
+        }
+    }
+}
