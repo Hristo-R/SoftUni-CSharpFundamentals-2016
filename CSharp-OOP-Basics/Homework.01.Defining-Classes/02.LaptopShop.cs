04500520a7f1632debcef5d83e51e0b78adcedb9
Hristo-R <hristo.razpopov@abv.bg>
2018-05-15 22:56:50 +0300

    added Homework.01 Defining Classes - Problem 2

---
+++ CSharp-OOP-Basics/Homework.01.Defining-Classes/02.LaptopShop.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem_02.LaptopShop
+{
+    using System;
+
+    public class LaptopShop
+    {
+        public static void Main()
+        {
+            Laptop laptopLevovo = new Laptop(
+                "Lenovo Yoga 2 Pro",
+                "Lenovo",
+                "Intel Core i5-4210U (2-core, 1.70 - 2.70 GHz, 3MB cache)",
+                "8 GB",
+                "Intel HD Graphics 4400",
+                "128GB SSD",
+                "13.3\" (33.78 cm) – 3200 x 1800 (QHD+), IPS sensor display",
+                new Battery("Li-Ion, 4-cells, 2550 mAh", "4,5"),
+                2259.00M);
+
+            Laptop laptopHP = new Laptop("HP 250 G2", 699.00M);
+
+            Console.WriteLine(laptopLevovo);
+            Console.WriteLine();
+            Console.WriteLine(laptopHP);
+        }
+    }
+}
