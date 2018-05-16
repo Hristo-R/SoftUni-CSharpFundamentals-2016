8a862a60662a75bc1a9fc75a898ba65355c126f8
Hristo-R <hristo.razpopov@abv.bg>
2018-05-16 17:10:39 +0300

    renamed folder

---
+++ CSharpOOP-Basics/Homework.01.Defining-Classes/03.Component.cs
@@ -0,0 +1,76 @@
+﻿namespace Problem03.PC_Catalog
+{
+    using System;
+
+    public class Component
+    {
+        private string name;
+        private string details;
+        private decimal price;
+
+        public Component(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Component(string name, string details, decimal price) : this(name, price)
+        {
+            this.Details = details;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public string Details
+        {
+            get
+            {
+                return this.details;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.details = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.price = value;
+            }
+        }
+    }
+}
---
+++ CSharpOOP-Basics/Homework.01.Defining-Classes/03.Computer.cs
@@ -0,0 +1,92 @@
+﻿namespace Problem03.PC_Catalog
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Computer
+    {
+        private string computerName;
+        private List<Component> components;
+        private decimal computerPrice;
+
+        public Computer(string computerName, List<Component> components)
+        {
+            this.ComputerName = computerName;
+            this.Components = components;
+        }
+
+        public string ComputerName
+        {
+            get
+            {
+                return this.computerName;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.computerName = value;
+            }
+        }
+
+        public List<Component> Components
+        {
+            get
+            {
+                return this.components;
+            }
+
+            set
+            {
+                this.components = value;
+            }
+        }
+
+        public decimal ComputerPrice
+        {
+            get
+            {
+                return this.computerPrice;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Input data is invalid");
+                }
+
+                this.computerPrice = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            decimal totalPrice = 0M;
+            StringBuilder computer = new StringBuilder();
+
+            computer.AppendLine($"computer name: {this.computerName}\n");
+            foreach (Component component in this.components)
+            {
+                computer.AppendLine($"component:");
+                computer.AppendLine($"\t {component.Name}");
+                if (component.Details != null)
+                {
+                    computer.AppendLine($"details:");
+                    computer.AppendLine($"\t {component.Details}\n");
+                }
+
+                totalPrice += component.Price;
+            }
+
+            computer.AppendLine($"\nTotal computer price: {totalPrice.ToString():F2} BGN");
+
+            return computer.ToString();
+        }
+    }
+}
