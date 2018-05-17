299768b7341c282c538499e6867960bc0297e822
Hristo-R <hristo.razpopov@abv.bg>
2018-05-17 10:31:03 +0300

    added Homework.02 Static Members and Namespaces/01.Point3D

---
+++ CSharpOOP-Basics/Homework.02.StaticMembersAndNamespaces/01.Point3D/Point3DMainProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace Point3D
+{
+    using System;
+
+    public class Point3DMainProgram
+    {
+        public static void Main()
+        {
+            var new3DPoint = new Point3D(1, 1, 1);
+            var start3DPoint = Point3D.StartPoint;
+
+            Console.WriteLine("3d point: {0}", new3DPoint);
+            Console.WriteLine("Start 3D point: {0}", start3DPoint);
+        }
+    }
+}
