8dbf78d06c3f5e71817d0a1e75734f92acae0c7f
Hristo-R <hristo.razpopov@abv.bg>
2018-05-17 10:43:42 +0300

    added Homework.02 StaticMembersAndNamespaces/02.DistanceCalculator

---
+++ CSharpOOP-Basics/Homework.02.StaticMembersAndNamespaces/02.DistanceCalculator/DistanceCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.DistanceCalculator
+{
+    using System;
+    using Point3D;
+
+    public class DistanceCalculator
+    {
+        public static double Calculator(Point3D firstPoint, Point3D secondPoint)
+        {
+            return Math.Sqrt(
+                ((secondPoint.X - firstPoint.X) * (secondPoint.X - firstPoint.X)) +
+                ((secondPoint.Y - firstPoint.Y) * (secondPoint.Y - firstPoint.Y)) +
+                ((secondPoint.Z - firstPoint.Z) * (secondPoint.Z - firstPoint.Z)));
+        }
+    }
+}
