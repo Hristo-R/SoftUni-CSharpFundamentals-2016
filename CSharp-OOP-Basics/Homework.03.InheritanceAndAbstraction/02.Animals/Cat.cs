64531687fdf74835c5c9f5c4af1dac0219b72b39
Hristo-R <hristo.razpopov@abv.bg>
2018-05-18 13:11:35 +0300

    added Homework.03.InheritanceAndAbstraction/02.Animals

---
+++ CSharp-OOP-Basics/Homework.03.InheritanceAndAbstraction/02.Animals/Cat.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.Animals
+{
+    using System;
+
+    public class Cat : Animals
+    {
+        public Cat(string name, int age, Gender gender)
+            : base(name, age, gender)
+        {
+        }
+
+        public override void ProduceSound()
+        {
+            Console.WriteLine("said miau!");
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Cat {0} is {1} year old, gender {2}", this.Name, this.Age, this.Gender);
+        }
+    }
+}
